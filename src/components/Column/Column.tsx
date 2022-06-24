62cbaa9a01b1945f43d017e94fbd092c1bd80e37
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2022-06-24 01:41:46 -0300

    feat: add props to Column component
    Add props flexbox and alignItems.

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
-import { border, BorderProps, borderRadius, BorderRadiusProps, color, ColorProps, layout, LayoutProps, space, SpaceProps } from 'styled-system';
+import { alignItems, border, BorderProps, borderRadius, BorderRadiusProps, color, ColorProps, flexbox, FlexboxProps, layout, LayoutProps, space, SpaceProps } from 'styled-system';
 
-type ColumnProps = LayoutProps & SpaceProps & ColorProps & BorderRadiusProps & BorderProps;
+type ColumnProps = LayoutProps & SpaceProps & ColorProps & BorderRadiusProps & BorderProps & FlexboxProps;
 
 export const Column = styled.div<ColumnProps>`
     display: flex ;
     flex-direction: column;
+    ${flexbox}
     ${layout}
     ${space}
     ${color}
     ${borderRadius}
     ${border}
+    ${alignItems}
 `;
