c4d9db76381d3883bcbdf8041c2111d9aee75a59
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2022-07-06 00:39:36 -0300

    feat: service that list, update and create tasks

---
+++ src/services/TodoService.ts
@@ -0,0 +1,17 @@
+import { AxiosResponse } from 'axios';
+import { ITodo } from 'interfaces';
+import { Api } from 'providers';
+
+const getAllTodos = (): Promise<AxiosResponse<ITodo[]>> => Api.get<ITodo[]>('/v1/todos');
+
+const createTodo = (todo: Pick<ITodo, 'task' | 'isDone'>): Promise<AxiosResponse<ITodo>> =>
+  Api.post<ITodo>('/v1/todos', todo);
+
+const updateTodo = (id: string, todo: Pick<ITodo, 'task' | 'isDone'>): Promise<AxiosResponse<ITodo>> =>
+  Api.put<ITodo>(`/v1/todos/${id}`, todo);
+
+export const TodoService = {
+  getAllTodos,
+  createTodo,
+  updateTodo
+};
