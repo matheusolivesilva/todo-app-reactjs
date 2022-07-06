6a2b1a433ed76d631ce3ab732835d8bd07f860d9
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2022-07-05 23:58:58 -0300

    feat: apply border when task is active with timer

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -15,11 +15,19 @@ import {
   SpaceProps,
 } from 'styled-system';
 
-type ColumnProps = LayoutProps & SpaceProps & ColorProps & BorderRadiusProps & BorderProps & FlexboxProps;
+type ColumnProps = LayoutProps &
+  SpaceProps &
+  ColorProps &
+  BorderRadiusProps &
+  BorderProps &
+  FlexboxProps & {
+    cursor?: string;
+  };
 
 export const Column = styled.div<ColumnProps>`
   display: flex;
   flex-direction: column;
+  ${({ cursor }): string | undefined => cursor && `cursor: ${cursor};`}
   ${flexbox}
   ${layout}
     ${space}
--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -1,14 +1,33 @@
-import { Column, Text } from 'components';
+import { Column, Text, Row, Icon } from 'components';
 import React from 'react';
 
 export type ListItemProps = {
-  label: string;
+  index: number;
+  id: string;
+  task: string;
+  isDone: number;
+  isActive: boolean;
+  onClick: (index: number) => void;
 };
 
-export const ListItem: React.FC<ListItemProps> = ({ label }) => {
+export const ListItem: React.FC<ListItemProps> = ({ index, task, isDone, isActive, onClick }) => {
   return (
-    <Column width="100%" bg="rgba(0, 0, 0, 0.2)" p="20px" borderRadius="4px" mb="10px" borderLeft="5px solid #fff">
-      <Text>{label}</Text>
+    <Column
+      width="100%"
+      bg="rgba(0, 0, 0, 0.2)"
+      p="20px"
+      borderRadius="4px"
+      mb="10px"
+      cursor="pointer"
+      borderLeftWidth="5px"
+      borderLeftStyle="solid"
+      borderLeftColor={isActive ? '#fff' : 'transparent'}
+      onClick={(): void => onClick(index)}
+    >
+      <Row>
+        <Text flex={1}>{task}</Text>
+        {isDone === 1 && <Icon variant="done-white" />}
+      </Row>
     </Column>
   );
 };
